2b3bbc64db3b8c3aad1350d282db30f48844ce32
Aaron Fisher <fisher@jhu.edu>
2015-08-19 22:30:12 -0400

    adding indentation

--- call-jags-noninf-obs.R
+++ call-jags-noninf-obs.R
@@ -146,22 +146,26 @@ jags_data<-list(K=K,
 #note that not all "parameters" need to be initialized. specifically, don't initialize random effects, but do need to set initial values for mean and covariance of random effects
 # also need to set initial values for latent variables that are not observed (here, eta)
 
-inits <- function() {
-	
-p_eta<-rbeta(1,1,1)
+inits <- function(){
+		
+	p_eta<-rbeta(1,1,1)
 
-eta.hat<-pt.data$rc[is.na(eta.data)]
+	eta.hat<-pt.data$rc[is.na(eta.data)]
 
-xi<-c(min(rlnorm(1),100), min(rlnorm(1),100))
-mu_raw<-as.matrix(cbind(rnorm(d.Z),rnorm(d.Z)))
-Tau_B_raw<-rwishart((d.Z+1),diag(d.Z)*var_vec)$W
-sigma_res<-min(rlnorm(1),1)
+	xi<-c(min(rlnorm(1),100), min(rlnorm(1),100))
+	mu_raw<-as.matrix(cbind(rnorm(d.Z),rnorm(d.Z)))
+	Tau_B_raw<-rwishart((d.Z+1),diag(d.Z)*var_vec)$W
+	sigma_res<-min(rlnorm(1),1)
 
-beta<-rnorm(d.X)
+	beta<-rnorm(d.X)
 
-gamma.RC<-rnorm((d.W.RC+1),mean=0,sd=0.1)
+	gamma.BX<-rnorm((d.W.BX+1), mean=0, sd=0.1) #last coefficient is effect of eta=1
+	gamma.RC<-rnorm((d.W.RC+1), mean=0, sd=0.1) #ditto
+	gamma.RRP<-c(rnorm((d.W.RRP+2), mean=0, sd=0.01))  #here, include interaction with last prediction and eta=1
 
-list(p_eta=p_eta, eta.hat=eta.hat, xi=xi, mu_raw=mu_raw, Tau_B_raw=Tau_B_raw, sigma_res=sigma_res, beta=beta, gamma.RC=gamma.RC) }
+	list(p_eta=p_eta, eta.hat=eta.hat, xi=xi, mu_raw=mu_raw, Tau_B_raw=Tau_B_raw, sigma_res=sigma_res, beta=beta, gamma.BX=gamma.BX, gamma.RC=gamma.RC, gamma.RRP=gamma.RRP)
+}
+
 
 
 # parameters to track
