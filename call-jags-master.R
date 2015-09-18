2f832a771ad33c3a9ca8a03936130caf3db8b9b2
Aaron Fisher <fisher@jhu.edu>
2015-09-18 12:41:11 -0400

    adding options for IOP_BX and IOP_RRP; other minor changes

--- call-jags-master.R
+++ call-jags-master.R
@@ -1,6 +1,8 @@
-#rm(list=ls())
-#setwd("/Users/ryc/Dropbox/inhealth/prediction-model-final/sim-data")
-#setwd("/Users/ryc/GitHub/prostate_surveillance")
+
+# User-specific, non-generalizable code
+# rm(list=ls())
+# setwd("/Users/ryc/Dropbox/inhealth/prediction-model-final/sim-data")
+# setwd("/Users/ryc/GitHub/prostate_surveillance")
 # setwd("/Users/aaronfisher/Dropbox/Future Projects/inHealth Prostate Screening/repo")
 # setwd("/home/bst/student/afisher/inHealth_prostate")
 
@@ -20,7 +22,13 @@ library("splines")
 library("dplyr")
 
 
-#get data
+
+
+
+#######################################
+#######################################
+#Load & filter data
+
 pt.data<-read.csv("simulation-data/pt-data-sim.csv")
 psa.data.all<-read.csv("simulation-data/psa-data-sim.csv")
 data.use.all<-read.csv("simulation-data/bx-data-sim.csv")
@@ -28,19 +36,33 @@ data.use.all<-read.csv("simulation-data/bx-data-sim.csv")
 #data frames with '.all' suffix will be cropped in some cases to for "leave one out" JAGS fits.
 
 #Crop out data for "leave-one-out" model fits.
-data.use.star<- filter(data.use.all, subj==star)
+data.use.star<- dplyr::filter(data.use.all, subj==star)
 bx.inds <- which(data.use.star$bx.here==1)
 last.bx.ind <- bx.inds[length(bx.inds)-1] #Take all data since the 2nd to last biopsy as "new data."
 last.age <- data.use.star$age[last.bx.ind]
 
 
 if(!crop | length(last.age)==0 ) last.age <- 0 #there could be some PSA measurements before you're enrolled in this biopsy study.
-psa.data <- filter(psa.data.all, !(subj==star & age>=last.age))
-data.use <- filter(data.use.all, !(subj==star & age>=last.age))
+psa.data <- dplyr::filter(psa.data.all, !(subj==star & age>=last.age))
+data.use <- dplyr::filter(data.use.all, !(subj==star & age>=last.age))
+
+
+
+
+(n<-dim(pt.data)[1]) #there are 1000 patients
+#This matrix will always be fully intact, but psa and bx data may not be.
+
+
+
+
+#######################################
+#######################################
+#Generate covariates and datavectors to be sent to JAGS
 
 
 
 #function to get natrual spline basis
+#to be used if IOP_BX or IOP_RRP is true
 get.ns.basis<-function(obs.data,knots){
 #	knots<-quantile(obs.data,p=c(0.25,0.5,0.75))
 	od.k1<- obs.data-knots[1]
@@ -52,10 +74,7 @@ get.ns.basis<-function(obs.data,knots){
 	return(as.vector((od.k1^3 - od.k3^3)/(knots[3]-knots[1]) - (od.k2^3 - od.k3^3)/(knots[3]-knots[2])))}
 
 
-#Before call to JAGS, get the data into simple matrices and vectors to send to JAGS
 
-(n<-dim(pt.data)[1]) #there are 1000 patients
-#This matrix will always be fully intact, but psa and bx data may not be.
 
 #get observed latent class
 eta.data<-pt.data$obs.eta
@@ -63,8 +82,9 @@ table(eta.data) #107 in each
 (n_eta_known<-sum(!is.na(eta.data))) #214
 
 
+#########
+# PSA model
 
-#PSA model
 (n_obs_psa<-dim(psa.data)[1])
 Y<-psa.data$log.psa
 summary(Y)
@@ -79,24 +99,12 @@ round(apply(Z.data,2,summary),2)
 X.data<-as.matrix(cbind(psa.data$std.vol))
 (d.X<-dim(X.data)[2])
 summary(X.data)
+#########
 
 
-###bx data
+#########
+# Reclassification (RC) logistic regression outcome model
 
-#observation model
-bx.data<-data.use[!is.na(data.use$bx.here),] #remove patients who have already had RC observed but haven't had surgery or been censored
-(n_bx<-dim(bx.data)[1])
-BX<-as.numeric(bx.data$bx.here) #indicator of bx
-subj_bx<-bx.data$subj
-
-
-W.BX.data<-as.matrix(cbind(rep(1,n_bx), bx.data$age.std, bx.data$age.ns, ns(bx.data$time,4), bx.data$num.prev.bx, ns(bx.data$sec.time.std,4)  ))
-(d.W.BX<-dim(W.BX.data)[2]) #12
-round(apply(W.BX.data,2,summary),2)
-
-
-
-#outcome model (logistic regression for reclassification)
 rc.data<-data.use[data.use$bx.here==1 & !is.na(data.use$bx.here),] #only use records where a biopsy occurred
 (n_rc<-dim(rc.data)[1])
 RC<-as.numeric(rc.data$rc)
@@ -106,25 +114,70 @@ subj_rc<-rc.data$subj
 W.RC.data<-as.matrix(cbind(rep(1,n_rc),  rc.data$age.std, rc.data$time, rc.data$time.ns, rc.data$sec.time.std ))
 (d.W.RC<-dim(W.RC.data)[2])
 round(apply(W.RC.data,2,summary) ,2)
+#########
 
 
 
+#########
+#biopsy (BX) logistic regression observation model
+
+#If these are NULL, they effectively won't be in the list passed to JAGS
+bx.data <-
+n_bx <-
+BX <-
+subj_bx<-
+W.BX.data<-
+d.W.BX <- NULL
+
+if(IOP_BX){
+	bx.data<-data.use[!is.na(data.use$bx.here),] # remove patients who have already had RC observed but haven't had surgery or been censored
+	(n_bx<-dim(bx.data)[1])
+	BX<-as.numeric(bx.data$bx.here) #indicator of bx
+	subj_bx<-bx.data$subj
+
+
+	W.BX.data<-as.matrix(cbind(rep(1,n_bx), bx.data$age.std, bx.data$age.ns, ns(bx.data$time,4), bx.data$num.prev.bx, ns(bx.data$sec.time.std,4)  ))
+	(d.W.BX<-dim(W.BX.data)[2]) 
+	round(apply(W.BX.data,2,summary),2)
+}
+#########
+
+
+
+
+#########
+# Surgery (RRP) logistic regression observation model
 #logistic regression for RRP
 #this uses all records, because patients always at risk of choosing surgery
-RRP<-as.numeric(data.use$rrp)
-(n_rrp<-dim(data.use)[1])
-subj_rrp<-data.use$subj
+
+RRP<-
+n_rrp<-
+subj_rrp<-
+W.RRP.data<-
+d.W.RRP<- NULL
+
+if(IOP_RRP){
+	RRP<-as.numeric(data.use$rrp)
+	(n_rrp<-dim(data.use)[1])
+	subj_rrp<-data.use$subj
 
 
-W.RRP.data<-as.matrix(cbind(rep(1,n_rrp), data.use$age.std, data.use$age.ns, ns(data.use$time,4), ns(data.use$sec.time.std,3) , data.use$num.prev.bx.rrp, data.use$prev.G7)) #
-(d.W.RRP<-dim(W.RRP.data)[2])
-round(apply(W.RRP.data,2,summary) ,2)
+	W.RRP.data<-as.matrix(cbind(rep(1,n_rrp), data.use$age.std, data.use$age.ns, ns(data.use$time,4), ns(data.use$sec.time.std,3) , data.use$num.prev.bx.rrp, data.use$prev.G7)) #
+	(d.W.RRP<-dim(W.RRP.data)[2])
+	round(apply(W.RRP.data,2,summary) ,2)
+}
+#########
 
 
-##get starting values, other functions necessary for call to JAGS
 
-#lmer fit for initializing parameters
-#do this to get the starting value for a variance paramter in JAGS
+
+
+#######################################
+#######################################
+# Initialize parameters for JAGS
+# 	(and other JAGS helper functions)
+
+#lmer fit for initializing variance parameter in JAGS
 mod.lmer<-lmer(log.psa~ std.vol + (1+ age.std |id), data=psa.data)
 (var_vec <- apply(coef(mod.lmer)$id, 2, var)[1:d.Z])
 (var_vec <- c(var_vec[2], var_vec[1]))
@@ -132,7 +185,15 @@ mod.lmer<-lmer(log.psa~ std.vol + (1+ age.std |id), data=psa.data)
 #bundle data for call to JAGS
 #this is observed data and constant variables that have already been assigned values (e.g. number of class K=2, number of subjects n, etc.)
 K<-2
-jags_data<-list(K=K, n=n, eta.data=eta.data, n_eta_known=n_eta_known, n_obs_psa=n_obs_psa, Y=Y, subj_psa=subj_psa, Z=Z.data, X=X.data, d.Z=d.Z, d.X=d.X, I_d.Z=diag(d.Z), BX=BX, n_bx=n_bx, subj_bx=subj_bx, W.BX=W.BX.data, d.W.BX=d.W.BX, RC=RC, n_rc=n_rc, subj_rc=subj_rc, W.RC=W.RC.data, d.W.RC=d.W.RC, RRP=RRP, n_rrp=n_rrp, subj_rrp=subj_rrp, W.RRP=W.RRP.data, d.W.RRP=d.W.RRP)
+
+jags_data<-list(K=K, n=n, eta.data=eta.data, n_eta_known=n_eta_known,
+	Y=Y, subj_psa=subj_psa, Z=Z.data, X=X.data, d.Z=d.Z, d.X=d.X, I_d.Z=diag(d.Z), n_obs_psa=n_obs_psa,
+	RC=RC, n_rc=n_rc, subj_rc=subj_rc, W.RC=W.RC.data, d.W.RC=d.W.RC)
+
+if(IOP_BX) jags_data <- c(jags_data,
+		list(BX=BX, n_bx=n_bx, subj_bx=subj_bx, W.BX=W.BX.data, d.W.BX=d.W.BX))
+if(IOP_RRP) jags_data <- c(jags_data, 
+		list(RRP=RRP, n_rrp=n_rrp, subj_rrp=subj_rrp, W.RRP=W.RRP.data, d.W.RRP=d.W.RRP))
 
 
 #initialize model
@@ -153,25 +214,50 @@ inits <- function(){
 
 	beta<-rnorm(d.X)
 
-	gamma.BX<-rnorm((d.W.BX+1), mean=0, sd=0.1) #last coefficient is effect of eta=1
-	gamma.RC<-rnorm((d.W.RC+1), mean=0, sd=0.1) #ditto
-	gamma.RRP<-c(rnorm((d.W.RRP+2), mean=0, sd=0.01))  #here, include interaction with last prediction and eta=1
+	gamma.RC<-rnorm((d.W.RC+1), mean=0, sd=0.1) #last coefficient is effect of eta=1
 
-	list(p_eta=p_eta, eta.hat=eta.hat, xi=xi, mu_raw=mu_raw, Tau_B_raw=Tau_B_raw, sigma_res=sigma_res, beta=beta, gamma.BX=gamma.BX, gamma.RC=gamma.RC, gamma.RRP=gamma.RRP)
+	gamma.BX <- 
+	gamma.RRP <- NULL
+	if(IOP_BX) gamma.BX<-rnorm((d.W.BX+1), mean=0, sd=0.1) #last coefficient is effect of eta=1
+	if(IOP_RRP) gamma.RRP<-c(rnorm((d.W.RRP+2), mean=0, sd=0.01))  #here, include interaction with last prediction and eta=1
+
+	out<-list(p_eta=p_eta, eta.hat=eta.hat, xi=xi, mu_raw=mu_raw, Tau_B_raw=Tau_B_raw, sigma_res=sigma_res, beta=beta,
+		gamma.RC=gamma.RC)
+	if(IOP_BX) out$gamma.BX <- gamma.BX
+	if(IOP_RRP) out$gamma.RRP <- gamma.RRP
+	
+	out
+
 }
 
 
 
 # parameters to track
-params <- c("p_eta", "eta.hat", "mu_int", "mu_slope", "sigma_int", "sigma_slope", "sigma_res", "rho_int_slope", "cov_int_slope", "b.vec", "beta", "gamma.BX", "gamma.RC", "gamma.RRP", "p_bx", "p_rc", "p_rrp")  #you may not need to monitor p_bx, p_rc, and p_rrp. taking them out of the list should improve computing time a bit
+params <- c("p_eta", "eta.hat", "mu_int", "mu_slope", "sigma_int", "sigma_slope", "sigma_res", "rho_int_slope", "cov_int_slope", "b.vec", "beta",
+	"gamma.RC",
+	"p_rc")
+if(IOP_BX) params <- c(params, "gamma.BX", "p_bx")
+if(IOP_RRP) params <- c(params, "gamma.RRP", "p_rrp")	
+#you may not need to monitor p_bx, p_rc, and p_rrp. taking them out of the list should improve computing time a bit
 
 # MCMC settings
 #ni, nb, nt, and nc are now set in separate files.
 
 
+model.file.IOP <- paste0(
+	'model-for-jags-',
+	c('N')[!IOP_BX],'IOP_BX-',
+	c('N')[!IOP_RRP],'IOP_RRP',
+	'.txt'
+	)
+
 do.one<-function(seed){
 	set.seed(seed)	
-	outj<-jags(jags_data, inits=inits, parameters.to.save=params, model.file="model-for-jags-inf-obs.txt", n.thin=nt, n.chains=nc, n.burnin=nb, n.iter=ni)
+	outj<-jags(jags_data,
+		inits=inits,
+		parameters.to.save=params,
+		model.file=model.file.IOP,
+		n.thin=nt, n.chains=nc, n.burnin=nb, n.iter=ni)
 
 	return(outj$BUGSoutput)
 }
