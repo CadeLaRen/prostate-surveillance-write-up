10bbb739f2de95b8d26a69179257b3db052485dc
Aaron Fisher <fisher@jhu.edu>
2015-10-20 13:42:50 -0400

    cleaning up IS combine script

--- IS-combine-results.r
+++ IS-combine-results.r
@@ -5,6 +5,7 @@ library(ggplot2)
 IOP_BX <- TRUE #Informative observation process for biopsy
 IOP_SURG <- TRUE #Informative observation process for surgery
 leave_one_out <- FALSE
+crop<-FALSE
 
 IOPs<-paste0(
 	c('N')[!IOP_BX],'IOP_BX-',
@@ -13,9 +14,13 @@ batch_path<-paste0(
 	'batches/',
 	IOPs,
 	'/leave_one_out_',leave_one_out,
+	'/crop_',crop,
 	'/batch-1/')
 posterior_path<-paste0(batch_path,
 	'concatenated_posterior.rds')
+
+
+
 
 ###############
 # Load Data
@@ -27,16 +32,9 @@ bx_data_full <- read.csv("simulation-data/bx-data-sim.csv")
 
 
 
-### Output from leave-one-Out JAGS model  (output-out -> oo)
-	# For now just use the output from the full model as an approximate replacement
-### Then collect posterior estimates from full JAGS model
-# of <- readRDS('batches/inf-obs/1/2015-07-24_JAGS_full_sample_summarized_12.rds')
-oo <- readRDS(posterior_path)
-of <- readRDS(posterior_path)
 
-
-
-missing_etas <- which(is.na(pt_data_full$obs_eta)) #=1 if observed and aggressive, 0 if observed and not aggressive, or NA if not observed
+of <- readRDS(posterior_path) # Output from full model
+missing_etas <- which(is.na(pt_data_full$obs_eta)) #obs_eta=1 if observed and aggressive, 0 if observed and not aggressive, or NA if not observed
 eta_true_or_jags<-pt_data_full$obs_eta
 eta_true_or_jags[missing_etas]<-colMeans(of$eta_hat_means) #indexed by subject
 
@@ -44,75 +42,103 @@ nsim <- dim(of1<-of$eta_hat_means)[1]
 eta_jags1<-colMeans(of$eta_hat_means[1:nsim <  nsim/2,])
 eta_jags2<-colMeans(of$eta_hat_means[1:nsim >= nsim/2,])
 quantile(abs(eta_jags1-eta_jags2),prob=c(.5,.9,.99,1))
-# From two MCMC samples the concordance is high. About 10 times higher than what we get with IS.
+	# Note - from "two" MCMC samples the concordance is very high. About 10 times higher than what we get with IS.
+#############
 
-load(paste0(batch_path,'/2015-10-02_seed_101_IOP_BX-TRUE_IOP_SURG-TRUE_P-5e+06_online_fit_small_results.RData'))
 
-if(FALSE){
+#############
+# Combine results from parallel jobs
+
+load(paste0(batch_path,'/2015-10-16_seed_101_IOP_BX-TRUE_IOP_SURG-TRUE_P-5e+06_online_fit_results.RData')) #small & dynamic matrices
+
 
 files <- dir(batch_path)
 fitfiles <- files[grep('online',files)]
 
-small$draw_type <- 'small'
-dynamic$draw_type <- 'dynamic'
-
-bigConcatenated <- data.frame()
+bigAll <- data.frame()
 bigfiles <- files[grep('big',files)]
 for(i in 1:length(bigfiles)){
 
 	big_i <- readRDS(paste(batch_path,bigfiles[i],sep='/'))
+	bigAll <- rbind(bigAll,big_i)
 
 }
+bigAll <- bigAll[order(bigAll$subj),]
+as.tbl(bigAll)
 
-}
 
 	
 
-big<-readRDS(paste0(batch_path,'2015-10-03_seed_101_IOP_BX-TRUE_IOP_SURG-TRUE_P-5e+06_online_fit_big_combined.rds'))
 small$draw_type<-'small'
 dynamic$draw_type<-'dynamic'
-big$draw_type<-'big'
+bigAll$draw_type<-'big'
 
 
-quantile(dynamic$time_IS,prob=c(.5,.9,.95,.99,1))
 
-ofits<-rbind(small,dynamic,big) #out of sample fits
+ofits<-rbind(small,dynamic,bigAll) #out of sample fits
 
 ofits$etas_jags<- eta_true_or_jags[ofits$subj]
 ofits<-mutate(ofits, squared_errors_IS = (etas_jags-etas_IS)^2)
-ofits<-mutate(ofits, errors_zhenke_abs = sqrt((etas_jags-etas_zhenke)^2) )
+ofits<-mutate(ofits, errors_ZW_abs = sqrt((etas_jags-etas_ZW)^2) )
 
 ofits<-mutate(ofits, errors_IS_abs = sqrt((etas_jags-etas_IS)^2) )
-ofits<-mutate(ofits, errors_zhenke_abs = sqrt((etas_jags-etas_zhenke)^2) )
+ofits<-mutate(ofits, errors_ZW_abs = sqrt((etas_jags-etas_ZW)^2) )
+
+# saveRDS(ofits,file=paste0(batch_path,'/ISfitsConcatenated.rds'))
+# ofits<-readRDS(paste0(batch_path,'/ISfitsConcatenated.rds'))
+#############
 
 
-ggplot(ofits[ofits$draw_type=='small',]) +
-	geom_point(aes(x=effective_ss, y=errors_IS_abs),cex=1.5) +
-	scale_x_sqrt()+
-	theme(text=element_text(size=18)) +
-	labs(title='Absolute Error v. Calculation Time',x='Effective sample size\n(square root spacing)',y='Absolute difference', color='Candidate\ngenerating\nscheme')
-
-ggplot(ofits) + geom_point(aes(x=time_IS, y=errors_IS_abs,color=factor(draw_type,levels=c('big','dynamic','small'))),cex=1.5) +
-	scale_y_log10(breaks=c(.1,.05,.01,.005,.001)) +
-	scale_x_log10(breaks=c(10^(1:7)))+
-	theme(text=element_text(size=18)) +
-	labs(title='Absolute Error v. Calculation Time',x='Calculation time for IS\n(seconds, log spacing)',y='Absolute difference (log spacing)', color='Candidate\ngenerating\nscheme')
 
 
-ggplot(ofits) + geom_density(aes(x=time_IS, fill=draw_type),alpha=.6)+
-	# scale_y_log10(breaks=c(.1,.05,.01,.005,.001)) +
-	scale_x_log10(breaks=c(10^(1:7)))+
-	theme(text=element_text(size=18)) +
-	labs(title='Calculation Time',x='Calculation time for IS\n(seconds, log spacing)', color='Candidate\ngenerating\nscheme')
+#############
+#Explore results
 
-png(paste0('plots/',Sys.Date(),'_error_by_method.png'),height=400,width=460,pointsize=16)
-ggplot(ofits) + geom_violin(aes(x=factor(draw_type,levels=c('small','dynamic','big')), y=errors_IS_abs),cex=1.5)+
-	labs(title='Absolute Error v. Sampling Scheme',x='Candidate Generating Scheme',y='Absolute Difference')+
-	theme(text=element_text(size=18))
-dev.off()
+timeq<-c(0,.05,.25,.75,.9,.95,1)
+print(data.frame(
+	small=quantile(filter(ofits, draw_type=='small')$time_IS,
+		probs=timeq),
+	big=quantile(filter(ofits, draw_type=='big')$time_IS,
+		probs=timeq)/60,
+	dynamicSec=quantile(filter(ofits, draw_type=='dynamic')$time_IS,
+		probs=timeq),
+	dynamicMin=quantile(filter(ofits, draw_type=='dynamic')$time_IS,
+		probs=timeq)/60
+))
 
-# png(paste0('plots/',Sys.Date(),'_ESS_error_log.png'),height=400,width=460,pointsize=16)
-ggplot(ofits) + geom_point(aes(x=effective_ss, y=errors_IS_abs,color=draw_type),cex=0.8) +
+
+#root mean squared error for IS & RS
+filter(ofits, draw_type=='dynamic')%>% 
+	transmute(sq_err=(etas_jags-etas_IS)^2) %>%
+	summarize(mean(sq_err)) %>%
+	sqrt
+filter(ofits, draw_type=='dynamic')%>%
+	transmute(sq_err=(etas_jags-etas_RS)^2) %>%
+	summarize(mean(sq_err)) %>%
+	sqrt
+
+
+#Distribution of errors
+errorq<-c(.5,.8,.9,.95,.99,1)
+print(data.frame(
+	'dynamic'=quantile(filter(ofits, draw_type=='dynamic')$errors_IS_abs,
+		probs=errorq),
+	'small'=quantile(filter(ofits, draw_type=='small')$errors_IS_abs,
+		probs=errorq),
+	'big'=quantile(filter(ofits, draw_type=='big')$errors_IS_abs,
+		probs=errorq)
+))
+
+
+#How many particles ended up being drawn?
+range(filter(ofits, draw_type=='big')$particle_draws)
+range(filter(ofits, draw_type=='small')$particle_draws)
+range(filter(ofits, draw_type=='dynamic')$particle_draws)
+
+
+
+# png(paste0('plots/',Sys.Date(),'_ESS_error_log.png'),height=450,width=510,pointsize=19,type='cairo')
+ggplot(ofits) + geom_point(aes(x=effective_ss, y=errors_IS_abs,color=draw_type),cex=1.1) +
 	scale_y_log10(breaks=c(.1,.05,.01,.005,.001)) +
 	scale_x_log10(breaks=c(10^c(1:7)))+
 	theme(text=element_text(size=18)) +
@@ -121,40 +147,7 @@ ggplot(ofits) + geom_point(aes(x=effective_ss, y=errors_IS_abs,color=draw_type),
 	theme(axis.text.x=element_text(angle=35, hjust = 1))
 dev.off()
 
-ggplot(ofits[ofits$draw_type!='big',]) + geom_line(aes(x=effective_ss, y=errors_IS_abs, group=subj)) +
-	# scale_y_log10(breaks=c(.1,.05,.01,.005,.001)) +
-	scale_x_log10(breaks=c(10^(1:7)))+
-	labs(title='Effective Sample Size v. Absolute Error',x='Effective sample size for IS (log spacing)',y='Absolute difference (log spacing)', color='Candidate\ngenerating\nscheme') +
-	geom_vline(xintercept=1000,lty=2)
-
-group_by(ofits,draw_type) %>%
-	summarize(mean(errors_IS_abs))
-
-
-ggplot(ofits) + geom_point(aes(x=effective_ss, y=squared_errors_IS,color=draw_type),cex=0.9) +
-	geom_vline(xintercept=1000,lty=2)+
-	theme(text=element_text(size=18)) +
-	labs(title='Effective Sample Size v. Absolute Error',x='Effective sample size for IS\n(sqrt spacing)',y='Absolute difference', color='Candidate\ngenerating\nscheme')+
-	theme(axis.text.x=element_text(angle=35, hjust = 1))
-
-
-# png(paste0('plots/',Sys.Date(),'_ESS_error_sqrt.png'),height=440,width=450,pointsize=16)
-ggplot(ofits) + geom_point(aes(x=effective_ss, y=errors_IS_abs,color=draw_type),cex=0.9) +
-	scale_x_sqrt()+
-	geom_vline(xintercept=1000,lty=2)+
-	theme(text=element_text(size=18)) +
-	labs(title='Effective Sample Size v. Absolute Error',x='Effective sample size for IS\n(sqrt spacing)',y='Absolute difference', color='Candidate\ngenerating\nscheme')+
-	theme(axis.text.x=element_text(angle=35, hjust = 1))
-dev.off()
-
-ggplot(ofits[ofits$draw_type=='small',]) +
-	geom_point(aes(x=sqrt(effective_ss), y=errors_IS_abs),cex=1.5) +
-	# scale_y_log10(breaks=c(.1,.05,.01,.005,.001)) +
-	# scale_x_log10(breaks=c(10^(1:7)))+
-	theme(text=element_text(size=18)) +
-	labs(title='Effective Sample Size v. Absolute Error',x='sqrt(effective sample size for IS))',y='Absolute difference')
-
-# png(paste0('plots/',Sys.Date(),'_agreement_IS_MCMC.png'),height=400,width=450,pointsize=16)
+# png(paste0('plots/',Sys.Date(),'_agreement_IS_MCMC.png'),height=450,width=505,pointsize=19,type='cairo')
 ggplot(ofits)+geom_point(aes(x=etas_IS,y=etas_jags,color=draw_type),cex=1.2) +
 	theme(text=element_text(size=18)) +
 	labs(title='Agreement of risk estimates from\nIS and MCMC',x='Risk estimates from IS',y='Risk estimates from MCMC', color='Candidate\ngenerating\nscheme')
